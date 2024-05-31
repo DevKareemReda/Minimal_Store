bf8bc9b8219d9231657c70a44a5c036514ef7af0
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-05-31 14:53:09 -0700

    Created blog page and page read more

---
+++ js/blogMore.js
@@ -0,0 +1,163 @@
+const readMore = [
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post06.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Holisticly expedite customer directed",
+    date: "Saturday, 06 January 2023",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post03.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Credibly productivate corporate",
+    date: "Friday, 05 January 2023",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post02.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Energistically fabricate client-centered",
+    date: "Thursday, 04 January 2023",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post04.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Phosfluorescently seize excellent",
+    date: "Monday, 01 January 2018",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post05.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Synergistically redefine accurate",
+    date: "Wednesday, 03 January 2018",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post01.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Phosfluorescently incubate e-business",
+    date: "Tuesday, 02 January 2018",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in",
+  },
+];
+
+function showReadMore() {
+  let getTitle = new URLSearchParams(location.search).get("title");
+  let filterTitle = readMore.filter(
+    (el) => el.title.toLowerCase() === getTitle.split("/").join(" ")
+  );
+  filterTitle.map((el) => {
+    const { images, title, description, date, publish } = el;
+    let html = "";
+    html = `
+        <div class="col-md-8 text-center mb-5 mx-auto section">
+        <h3 class="main-head">${title}</h3>
+        </div>
+        <div class="col-md-12">
+        <div class="blogMore-img position-relative">
+        <img src="${images[0]}"
+        class="img-fluid" alt="Not Found">
+        <div class="blogMore-details d-flex align-items-center">
+        <img src="${images[1]}"
+        alt="Not Found">
+        <ul class="ms-4">
+        <li><a href="#">danny</a></li>
+        <li>${date} / ${publish} <a href="#">NEWS</a></li>
+        </ul>
+        </div>
+        </div>
+        <div class="blogMore-caption">
+        <h3 class="main-head">${title}</h3>
+        <p>${description[0]}</p>
+        <br>
+        <p>${description[1]}</p>
+        <br>
+        <p>${description[2]}</p>
+        <div class="social">
+        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="#"><i class="fa-brands fa-google-plus-g"></i></a>
+        <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+        <a href="#"><i class="fa-solid fa-envelope"></i></a>
+        </div>
+        </div>
+        </div>
+        `;
+    document.querySelector(".blog-more").innerHTML = html;
+  });
+}
+showReadMore();
+
+let getTitle = new URLSearchParams(location.search).get("title");
+let filterTitle = readMore.filter(
+  (el) => el.title.toLowerCase() !== getTitle.split("/").join(" ")
+);
+for (let i = 0; i < filterTitle.length - 2; i++) {
+  const { images, title } = filterTitle[i];
+  let html = "";
+  html = `
+    <div class="col-md-4">
+    <div class="related-inner">
+    <div class="related-img position-relative">
+    <img src="${images[0]}"
+    class="img-fluid" alt="Not Found">
+    </div>
+    <a href="blogMore.html?title=${title
+      .split(" ")
+      .join("/")
+      .toLowerCase()}">${title}</a>
+    </div>
+    </div>
+
+  `;
+  document.querySelector(".related-parent").innerHTML += html;
+}
---
+++ js/readMore.js
@@ -0,0 +1,114 @@
+const ReadMore = [
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post06.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Holisticly expedite customer directed",
+    date: "Saturday, 06 January 2023 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post03.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Credibly productivate corporate",
+    date: "Friday, 05 January 2023 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post02.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Energistically fabricate client-centered",
+    date: "Thursday, 04 January 2023 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post04.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Phosfluorescently seize excellent",
+    date: "Monday, 01 January 2018 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post05.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Synergistically redefine accurate",
+    date: "Wednesday, 03 January 2018 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+  {
+    images: [
+      "https://demo.kallyas.net/minimal-store/wp-content/uploads/sites/91/2018/01/post01.jpg",
+      "https://secure.gravatar.com/avatar/fffe06841f263355f3b6a99f4280ef49?s=46&d=mm&r=g",
+    ],
+    title: "Phosfluorescently incubate e-business",
+    date: "Tuesday, 02 January 2018 by danny",
+    description: [
+      "Objectively benchmark value-added niches via extensible communities. Efficiently administrate extensive methods of empowerment for tactical content. Dynamically reintermediate functionalized intellectual capital before cooperative process improvements. Energistically coordinate intuitive testing procedures rather than superior imperatives. Authoritatively monetize world-class models and global strategic theme areas.",
+      "Dramatically scale maintainable expertise via intermandated growth strategies. Authoritatively exploit premier customer service vis-a-vis emerging process improvements. Dramatically predominate team driven systems without unique human capital. Progressively leverage existing team driven products for efficient models. Authoritatively supply functional expertise via extensible functionalities.",
+      "Intrinsicly mesh bleeding-edge metrics without client-centric communities. Energistically incentivize resource maximizing deliverables whereas tactical infomediaries. Appropriately reinvent customized architectures rather than turnkey solutions. Proactively empower prospective e-commerce vis-a-vis orthogonal manufactured products. Synergistically expedite bleeding-edge functionalities via goal-oriented “outside the box” thinking.",
+    ],
+    publish: "Published in News",
+  },
+];
+
+ReadMore.map((el) => {
+  let html = "";
+  const {images, title, description} = el;
+  html = `
+    <div class="col-lg-4 col-sm-6">
+    <div class="blog-inner">
+    <div class="overlay-img">
+    <img src="${images[0]}" alt="post_01" class="img-fluid">
+    <div class="overlay-inner">
+    <a href="blogMore.html?title=${title.split(" ").join("/").toLowerCase()}" data-to="read more"></a>
+    </div>
+    </div>
+    <div class="blog-caption">
+    <h3 class="mt-2 mb-3">
+    <a href="#">
+    ${title}
+    </a>
+    </h3>
+    <span>Saturday, 06 January 2023 by <a href="#">danny</a></span>
+    <p class="my-4">${description[0].split(" ", 7).join(" ")}</p>
+    <span>Published in <a href="#">News</a></span>
+    </div>
+    </div>
+    </div>
+    `;
+    document.querySelector(".blog-parent").innerHTML += html;
+});
